b82173cc75c1e4553a4b6389c7671a60c88e6335
VISHAAL-KUMAR-A <js3441813@gmail.com>
2023-12-20 11:29:56 +0530

    two_models

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template, jsonify
+import numpy as np
+import joblib
+
+app = Flask(__name__)
+
+
+# Load the model
+regmodel = joblib.load('model.joblib')
+
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    data = np.array([list(data)])
+    print(data)
+    output = regmodel.predict(data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
